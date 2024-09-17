b07aceedee46f29ef0f395fbb8bfa2c94dc01743
fabrigabriell <150145958+fabrigabriell@users.noreply.github.com>
2024-09-16 22:23:21 -0300

    Add files via upload

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,38 +1,58 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import "./Navbar.css"
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link, NavLink } from "react-router-dom";
 import "../../App.css";
-import CartWidget from '../CartWidget/CartWidget';
-import { Link, NavLink } from 'react-router-dom';
-
-
+import CartWidget from "../CartWidget/CartWidget";
+import "./Navbar.css";
 
 function MyNav() {
   return (
     <header>
       <Navbar expand="lg" className="navbar-custom fixed-top">
         <Container>
-          <Link className="blinking-title" to={"/"}>GameHive</Link>
+          <Link className="blinking-title" to="/">
+            GameHive
+          </Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <li>
-              <Link to="/plataforma/PC">Pc</Link>
+              <li style={{ display: "inline-block", marginRight: "20px" }}>
+                <NavLink
+                  to="/plataforma/PC"
+                  className={({ isActive }) =>
+                    isActive ? "link active" : "link"
+                  }
+                  style={{ textDecoration: "none", color: "white" }}
+                >
+                  Pc
+                </NavLink>
               </li>
-
-              <li>
-              <Link to="/plataforma/Xbox">Xbox</Link>
+              <li style={{ display: "inline-block", marginRight: "20px" }}>
+                <NavLink
+                  to="/plataforma/Xbox"
+                  className={({ isActive }) =>
+                    isActive ? "link active" : "link"
+                  }
+                  style={{ textDecoration: "none", color: "white" }}
+                >
+                  Xbox
+                </NavLink>
               </li>
-
-              <li>
-              <Link to="/plataforma/PlayStation">Playstation</Link>
+              <li style={{ display: "inline-block" }}>
+                <NavLink
+                  to="/plataforma/PlayStation"
+                  className={({ isActive }) =>
+                    isActive ? "link active" : "link"
+                  }
+                  style={{ textDecoration: "none", color: "white" }}
+                >
+                  Playstation
+                </NavLink>
               </li>
-
             </Nav>
             <Nav className="ms-auto">
-            <CartWidget/>
+              <CartWidget />
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -42,4 +62,3 @@ function MyNav() {
 }
 
 export default MyNav;
-
